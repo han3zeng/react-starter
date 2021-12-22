a90c7075eef3fc5bf51ea5292b73a664b5b2b403
Han <allen0346@gmail.com>
2021-12-22 11:17:31 +0800

    Adjust: add setupTests for jest

--- src/components/Main.test.js
+++ src/components/Main.test.js
@@ -1,5 +1,4 @@
 /* eslint-disable react/jsx-filename-extension */
-import '@testing-library/jest-dom';
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Main from './Main';
